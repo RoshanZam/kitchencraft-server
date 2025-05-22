50414b75c18263fe7b30c0ecb3b62fd15e11a8d3
Roshan Zam <roshanzameer231@gmail.com>
2025-05-22 23:57:43 +0530

    initial commit

---
+++ src/main/java/com/example/kitchencraft/KitchencraftApplication.java
@@ -0,0 +1,14 @@
+package com.example.kitchencraft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+public class KitchencraftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KitchencraftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kitchencraft/KitchencraftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kitchencraft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KitchencraftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
